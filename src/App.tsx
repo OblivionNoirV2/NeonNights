67df26cba9c121c82ea256045ac45c7dcc5eb89c
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-21 11:23:03 -0500

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './sparkles.css';
+function App() {
+  return (
+    <div className='text-7xl'>
+      test
+    </div>
+  );
+}
+
+export default App;
