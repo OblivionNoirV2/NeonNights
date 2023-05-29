86d0831e7c345cc0bc11b62701828e0ba6847b7d
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-29 17:10:43 -0500

    added cart total

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -14,31 +14,40 @@ const Checkout = () => {
         <main className=" w-1/3 justify-center m-auto">
             <h1 className="text-6xl checkout">Cart</h1>
             <hr></hr>
-            {/*line up to the left, in as column*/}
-            <section className=" flex cart-page">
-                {cart.length == 0 ?
-                    <div className="flex text-4xl">
-                        Your cart is empty
-                    </div>
-                    :
-                    <ul className="flex flex-col justify-start ml-4 text-3xl">
-                        {unique_items.map((item, index) => (
-                            <li key={index} className="flex my-8 text-white">
-                                <img src={image_source_lookup[item]}
-                                    className="w-1/12 h-1/12">
-                                </img>
-                                <div className="flex flex-col ml-4">
-                                    {product_name_lookup[item]} x{cart.filter((i) => i === item).length}
-                                    <br></br>
-                                    <div className="text-2xl">
-                                        {getPrice(item)}
+            {/*line up to the left, in a column*/}
+            <section className="flex flex-col">
+                <section className=" flex cart-page">
+                    {cart.length == 0 ?
+                        <div className="flex text-4xl">
+                            Your cart is empty
+                        </div>
+                        :
+                        <ul className="flex flex-col justify-start ml-4 text-3xl">
+                            {unique_items.map((item, index) => (
+                                <li key={index} className="flex my-8 text-white">
+                                    <img src={image_source_lookup[item]}
+                                        className="w-1/12 h-1/12">
+                                    </img>
+                                    <div className="flex flex-col ml-4">
+                                        {product_name_lookup[item]} x{cart.filter((i) => i === item).length}
+                                        <br></br>
+                                        <div className="text-2xl">
+                                            {getPrice(item)}
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                }
+                                </li>
+
+                            ))}
+                        </ul>
+                    }
+                </section>
+                <section className="text-white">
+                    Total: ${Number(cart.reduce(
+                        (acc, item) => acc + getPrice(item), 0).toFixed(2)).toLocaleString('en-US', { minimumFractionDigits: 2 })}
+
+                </section>
             </section>
+            <hr></hr>
         </main>
     )
 }
