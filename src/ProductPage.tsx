dd6060eb64e0715e3fa875164eb76b54eb3d03fd
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-29 15:22:19 -0500

    Add proper product names to respective pages

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -14,7 +14,18 @@ function getProductData(item_number: number): ProductInfoProps | undefined {
         }
     }
 }
+const product_name_lookup: { [key: string]: string } = {
+    'ai': 'Aeon Plus AI Assistant',
+    'armor': 'Deflector X Laser Armor',
+    'katana': 'Shogun X Laser Katana',
+    'cpu': 'Nova Centauri 1024 core, 98.6 GHz CPU',
+    'gpu': 'Hyperion Z 9090zi GPU',
+    'glasses': 'VisionX Coding Glasses',
+    'pill': 'Prometheus Pill',
+    'chip': 'Pandora V4.0 Neural Chip',
+    'launcher': 'Rocket Launcher'
 
+}
 const ProductPage = () => {
     const { itemnumber } = useParams();
     const item_number_int = parseInt(itemnumber || '0');
@@ -33,7 +44,7 @@ const ProductPage = () => {
         <main className=" w-full ">
             <section className='flex flex-row bg-red-500 mx-48'>
                 <section className=' flex flex-col'>
-                    <h1 className='text-8xl '>{product_data.name}</h1>
+                    <h1 className='text-8xl '>{product_name_lookup[product_data.name]}</h1>
                     {
                         product_image &&
                         <img src={product_image} />
