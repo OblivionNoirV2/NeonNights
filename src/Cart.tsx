2977059ff4421cb80a2971a482a39b3e32288a9f
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-29 20:03:15 -0500

    backup

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -46,6 +46,7 @@ const CartElement = () => {
                                         title="Remove from cart">
                                         X
                                     </button>
+
                                 </li>
                             ))}
                         </ul>
