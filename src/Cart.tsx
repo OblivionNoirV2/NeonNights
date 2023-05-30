130956bd34bd1c3a588cf45581ae94a1b307aac8
OblivionNoirV2 <donahueb85@gmail.com>
2023-05-29 19:11:31 -0500

    added remove from cart option

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -4,14 +4,18 @@ import { product_name_lookup } from "./ProductInfo";
 import { CartContext } from "./Context";
 import { images_sources, image_source_lookup } from "./ProductInfo";
 import { getPrice } from "./ProductInfo";
-
+//todo: add notif when you add to cart
 const CartElement = () => {
     const { cart, setCart } = useContext(CartContext);
     //prevents listing the same item multiple times
     const unique_items = Array.from(new Set(cart));
-
+    function handleRemoval(item: string) {
+        //adjust the cart to be everything but the deleted item
+        const new_cart = cart.filter((i) => i !== item);
+        setCart(new_cart);
+    }
     return (
-        <main className=" w-1/3 justify-center m-auto">
+        <main className=" w-2/5 justify-center m-auto">
             <h1 className="text-6xl checkout">Cart</h1>
             <hr></hr>
             {/*line up to the left, in a column*/}
@@ -22,22 +26,27 @@ const CartElement = () => {
                             Your cart is empty
                         </div>
                         :
-                        <ul className="flex flex-col justify-start ml-4 text-3xl">
+                        <ul className="flex flex-col w-full justify-start ml-4 text-3xl">
                             {unique_items.map((item, index) => (
                                 <li key={index} className="flex my-8
                                  text-white cart-page">
                                     <img src={image_source_lookup[item]}
                                         className="w-1/12 h-1/12">
                                     </img>
-                                    <div className="flex flex-col ml-4">
+                                    <div className="flex flex-col w-full ml-4">
                                         {product_name_lookup[item]} x{cart.filter((i) => i === item).length}
                                         <br></br>
                                         <div className="text-2xl">
                                             {getPrice(item)}
                                         </div>
                                     </div>
+                                    <button onClick={() => handleRemoval(item)}
+                                        className="remove-btn flex 
+                                    h-fit px-2 py-1 rounded-lg"
+                                        title="Remove from cart">
+                                        X
+                                    </button>
                                 </li>
-
                             ))}
                         </ul>
                     }
