bdafd481d7ee8b181af440db6bef3ae2297b9f32
OblivionNoirV2 <donahueb85@gmail.com>
2023-06-01 11:47:17 -0500

    size adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,7 @@ const CartIcon = () => {
       </div>
       <Link to='/Cart'>
         <img src={require("./assets/cart.png")} alt="cart"
-          className="w-[6vmax]" />
+          className="lg:w-[6vmax] w-[8vmax]" />
       </Link>
     </div>
   );
@@ -180,7 +180,7 @@ const VolButton: React.FC = () => {
   }
 
   return (
-    <div className='sticky top-[9rem] z-10 w-full flex justify-end'>
+    <div className='sticky top-[9rem] text-xs lg:text-lg z-10 w-full flex justify-end'>
       <button onClick={toggleThemeSong}
         className='text-white mr-4 py-2 px-4 rounded-lg '>
         {isThemeSongOn ?
